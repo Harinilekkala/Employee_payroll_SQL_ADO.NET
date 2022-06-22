6c5ab95a3d160faa45ce9f5bad229ff96a1da9d5
Harinilekkala <harini.lekkala1997@gmail.com>
2022-06-22 18:35:50 +0530

    [Harini]: Connectivity Added

---
+++ Access_SQL_ADO.NET/Access_SQL_ADO.NET/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Access_SQL_ADO.NET;
+
+Employee_Repo select = new Employee_Repo();
+Payroll_Model model = new Payroll_Model();
+Console.WriteLine("1 - Establish Connectivity");
+int option = Convert.ToInt32(Console.ReadLine());
+switch (option)
+{
+    case 1:
+        select.GetPayroll();
+        break;
+
+}
